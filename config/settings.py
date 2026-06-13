8f70f35243675794edc7e20d1555114d17c9a17c
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-06-13 15:52:02 +0500

    first model, view and template

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tracker',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "staticfiles/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Day(models.Model):
+    SALAT = (
+        ("Fajr", "Fajr"),
+        ("Dhuhr", "Dhuhr"),
+        ("Asr", "Asr"),
+        ("Maghrib", "Maghrib"),
+        ("Isha", "Isha")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    salat = models.CharField(max_length=100, choices=SALAT)
+    on_time = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.salat} - {self.date}"
