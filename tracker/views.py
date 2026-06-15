470a1446b69e7eeb97a1b3e41f6b3834981a8e47
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-06-15 17:15:26 +0500

    template updated

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,16 @@
+from datetime import date
+
 from django.shortcuts import render
+from hijridate import Hijri, Gregorian
 
 def dashboard(request):
-    return render(request, "home.html")
+    today = date.today()
+    gregorian_date = Gregorian(today.year, today.month, today.day)
+    hijri_date = gregorian_date.to_hijri()
+    context = {
+        'hijri_date': hijri_date,
+        'hijri_month': hijri_date.month_name(),
+        'hijri_day': hijri_date.day,
+        'hijri_year': hijri_date.year,
+               }
+    return render(request, "home.html", context)
